7743d8fdfc6a8983e5bbf2dfe90332f8e2a14c28
afisman <afisman@gmail.com>
2022-07-12 16:35:03 +0200

    React contacts lab, pair programming

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import ContactCard from './Components/ContactCard';
+import contacts from './contacts.json'
+import { useState } from "react"
 
 function App() {
+  const restContacts = contacts.slice(5)
+  const fiveFirst = contacts.slice(0, 5)
+
+  const [randomContact, setRandomContact] = useState(fiveFirst)
+  const [orderAlphabetically, setOrderAlphabetically] = useState(fiveFirst)
+  const [popularityOrder, setPopularityOrder] = useState(fiveFirst)           //Started iteration 4 but didn´t finish
+
+  const fiveFirstCopy = [...randomContact]
+
+
+  const addRandom = () => {
+    let randomPos = restContacts[Math.floor(Math.random() * restContacts.length)]
+    fiveFirstCopy.unshift(randomPos)
+
+    setRandomContact(fiveFirstCopy)
+    console.log(randomContact)
+  }
+
+
+
+
+  const fiveBody = randomContact.map(contact => {
+    return <tbody>
+      <tr className='tCard'>
+        <th className='picture'>
+          <img
+            src={contact.pictureUrl}
+            alt={contact.name}
+            loading="lazy"
+          />
+        </th>
+        <th>{contact.name}</th>
+        <th>{contact.popularity}</th>
+        <th>{contact.wonOscar ? '🏆' : ''}</th>
+        <th>{contact.wonEmmy ? '🏆' : ''}</th>
+
+      </tr>
+    </tbody>
+
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={addRandom} >Sort by name</button>
+      <button onClick={addRandom} >Sort by popularity</button>
+      <button onClick={addRandom} >Add random contact</button>
+      <table>
+        <thead>
+          {/* <tr>
+            <th>IronContacts</th>
+          </tr> */}
+          <tr className='tTitles'>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+          </tr>
+        </thead>
+
+        {fiveBody}
+
+      </table>
     </div>
   );
 }
---
+++ src/Components/ContactCard.jsx
@@ -0,0 +1,29 @@
+// import './App.css';
+// import Contact from './contacts.json'
+
+// function ContactCard() {
+//     const fiveFirst = Contact.slice(0, 5)
+
+//     return (
+//         // <div className="App">
+//         //     <table>
+//         //         <thead>
+//         //             <tr>
+//         //                 <th>IronContacts</th>
+//         //             </tr>
+//         //             <tr>
+//         //                 <th>Picture</th>
+//         //                 <th>Name</th>
+//         //                 <th>Popularity</th>
+//         //                 <th>Won Oscar</th>
+//         //                 <th>Won Emmy</th>
+
+//         //             </tr>
+//         //         </thead>
+
+//         //     </table>
+//         // </div>
+//     );
+// }
+
+// export default ContactCard;
